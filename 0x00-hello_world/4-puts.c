11ee9c665579b403770692a9ec3d2ee3b704114e
faridsc <farid92.scorpio@gmail.com>
2023-02-16 21:47:27 +0100

    the task 5 file created

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * the main function of this program
+ */
+
+int main(int argc, char *argv[])
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
