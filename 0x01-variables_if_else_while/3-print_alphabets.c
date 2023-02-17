c7cfb11c34ce90903bb3faa2eecc51b375d3531a
faridsc <farid92.scorpio@gmail.com>
2023-02-17 16:36:44 +0100

    the task 4 file added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: 'the description of the main function'
+ *
+ * Return: 0  success
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A'; 
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
