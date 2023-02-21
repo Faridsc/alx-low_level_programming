7507a28995ad17fd6bf09c79d6730dca6b8bf5ad
faridsc <farid92.scorpio@gmail.com>
2023-02-22 00:45:01 +0100

    tesk 1 and a readme file were added without test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+ * main - the entry point 
+ * Return: 0 success
+ */
+
+int  main(void)
+{
+	char str[8] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# the *0x02. C - Functions, nested loops
+this project is for the nest loops and functions 
