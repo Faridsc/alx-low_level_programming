229a5f8547dc04f02ca2f8fb57d1b7663361df3f
faridsc <farid92.scorpio@gmail.com>
2023-02-23 13:29:52 +0100

    the header file betty warnnings and errors fixed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,7 +7,7 @@
  * Return: void
  * @i: int which to test if positive, negative or zero
  */
-void positive_or_negative(int);
+void positive_or_negative(int i);
 
 /**
  * positive_or_negative - print if an int is positive or negative
@@ -16,13 +16,13 @@ void positive_or_negative(int);
  */
 void positive_or_negative(int i)
 {
-        if (i < 0)
-                printf("%d is negative", i);
-        else if (i > 0)
-                printf("%d is positive", i);
-        else
-                printf("%d is zero", i);
-        printf("\n");
+	if (i < 0)
+		printf("%d is negative", i);
+	else if (i > 0)
+		printf("%d is positive", i);
+	else
+		printf("%d is zero", i);
+	printf("\n");
 }
 
 #endif
