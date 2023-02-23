5092682e7dc41757d521bcd8b2642ed7f007a23d
faridsc <farid92.scorpio@gmail.com>
2023-02-23 14:01:20 +0100

    task 1 fixed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,28 +1,8 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
-/**
- * positive_or_negative - print if an int is positive or negative
- * Return: void
- * @i: int which to test if positive, negative or zero
- */
 void positive_or_negative(int i);
 
-/**
- * positive_or_negative - print if an int is positive or negative
- * Return: void
- * @i: int which to test if positive, negative or zero
- */
-void positive_or_negative(int i)
-{
-	if (i < 0)
-		printf("%d is negative", i);
-	else if (i > 0)
-		printf("%d is positive", i);
-	else
-		printf("%d is zero", i);
-	printf("\n");
-}
-
-#endif
+#endif /* MAIN_H */
