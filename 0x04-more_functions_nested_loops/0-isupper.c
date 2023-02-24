14a4e192e8051f7c920d012135aaa7f6aabaddb4
faridsc <farid92.scorpio@gmail.com>
2023-02-24 20:10:54 +0100

    more functions and nested loops project first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+/**
+ * _isupper - a function to check if a char is in uppercase form
+ * @c: an int to check as a char
+ * Return: int; 1 if @c is uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 97 && c <= 122);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# *more_functions_nested_loops* project directopry
+> this directory is for the *more_functions_nested loops*
