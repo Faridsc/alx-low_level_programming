a75f1b27efc0c5aafc4d8c7f92d01a73a3ff5451
faridsc <farid92.scorpio@gmail.com>
2023-02-24 20:31:13 +0100

    main.h added to fixed the checker issue

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _isupper - checks if an int is uppercase as a char
+ * @c: an int to check
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+int _isupper(int c);
+#endif
