737cb0ce24b5cf932f9bf79b0e9f876f14923652
faridsc <farid92.scorpio@gmail.com>
2023-02-24 23:54:07 +0100

    task 4 file fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_numbers - prints the numbers from 0 to 9
  */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -35,8 +35,6 @@ void print_numbers(void);
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+
 #endif
