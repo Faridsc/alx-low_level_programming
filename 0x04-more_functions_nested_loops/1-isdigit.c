5d8753cfca8b30e3be3e7cc9112182c1d0cd569d
faridsc <farid92.scorpio@gmail.com>
2023-02-24 20:38:52 +0100

    the task 2 file is added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,9 @@
+/**
+ * _isdigit - a function to check if a char is in uppercase form
+ * @c: an int to check as a char
+ * Return: int; 1 if @c is uppercase 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
  * _isupper - checks if an int is uppercase as a char
  * @c: an int to check
  * Return: 1 if c is uppercase 0 otherwise
  */
 int _isupper(int c);
+
+/**
+ * _isdigit - checks if a char is a digit char
+ * @c: an int to check
+ * Return: 1 if @c is a digit 0 otherwise
+ */
+int _isdigit(int c);
+
 #endif
