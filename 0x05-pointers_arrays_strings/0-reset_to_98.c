1888031689178c085572de9c0ffe54fbb0a32f80
faridsc <farid92.scorpio@gmail.com>
2023-02-28 14:18:18 +0100

    the task 1 files are added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+  * reset_to_98 - reset the value of @n to 98
+  * @n: an int pointer to resets its value
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * _putchar - puts a char into the standard output
+  * @c: a char to print on the output
+  * Return: int 0 if successful
+  */
+int _putchar(char c);
+
+/**
+  * reset_to_98 - reset the value of @n to 98
+  * @n: an int pointer to the int value of @n
+  */
+void reset_to_98(int *n);
+
+#endif
