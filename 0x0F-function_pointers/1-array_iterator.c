5594f6570f2801b9d823bccb602a0f4d6b82c13b
faridsc <farid92.scorpio@gmail.com>
2023-03-22 18:20:59 +0100

    try to fixe some checker errors

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != NULL)
+	if (action != NULL && size >= 0)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
