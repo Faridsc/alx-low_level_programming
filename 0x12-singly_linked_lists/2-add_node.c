fb7c04d8d0368b4284d376c5d0c6ebaf5bef0da0
faridsc <farid92.scorpio@gmail.com>
2023-03-31 00:16:12 +0200

    task 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <strings.h>
+#include "defs.h"
+
+/**
+ * _strlen - returns the string length
+ * @s: the string to check
+ * Return: the string length as size_t
+ */
+size_t _strlen(char *s)
+{
+	char *ss;
+	size_t len;
+
+	len = 0;
+	ss = s;
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (*ss != '\0')
+	{
+		len++;
+		ss++;
+	}
+
+	return (len);
+}
+
+/**
+ * add_node - adds a node the list
+ * @head: list_t the list head
+ * @str: string of the node
+ * Return: the new add node or NULL
+ */
+list_t *add_node(list_t **head, char *str)
+{
+	list_t *node;
+
+	node = (list_t *)malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	node->len = _strlen(str);
+	node->str = str;
+	node->next = *head;
+	*head = node;
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,29 +12,6 @@ size_t print_list(const list_t *h);
 
 size_t list_len(const list_t *h);
 
-/**
- * _strlen - returns the string length
- * @s: the string to check
- * Return: the string length as size_t
- */
-size_t _strlen(char *s)
-{
-	char *ss;
-	size_t len;
-
-	len = 0;
-	ss = s;
-	if (s == NULL)
-	{
-		return (0);
-	}
-	while (*ss != '\0')
-	{
-		len++;
-		ss++;
-	}
-
-	return (len);
-}
+list_t *add_node(list_t **head, const char *str);
 
 #endif
