754c9084b4aca321d1a9ab8973b66f04922acd4a
faridsc <farid92.scorpio@gmail.com>
2023-03-31 02:09:01 +0200

    task 4 small betty style fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,17 +16,17 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	node = (list_t *)malloc(sizeof(list_t));
 	if (!node)
-		return NULL;
+		return (NULL);
 	node->len = strlen(str);
 	node->str = strdup(str);
 	node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = node;
-		return node;
+		return (node);
 	}
 	while (t->next != NULL)
 		t = t->next;
 	t->next = node;
-	return node;
+	return (node);
 }
