dde98bc65c5796e0ccacebc1062c695eb2600eef
faridsc <farid92.scorpio@gmail.com>
2023-03-31 03:16:46 +0200

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void first(void) __attribute__((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
