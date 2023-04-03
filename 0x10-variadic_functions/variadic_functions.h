e118751d7376046f9a6321a4a061cc0806819590
faridsc <farid92.scorpio@gmail.com>
2023-04-03 23:33:41 +0200

    task 4 fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,8 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
 
+void print_all(const char *const format, ...);
+
+char *formating(char c);
+
 #endif
