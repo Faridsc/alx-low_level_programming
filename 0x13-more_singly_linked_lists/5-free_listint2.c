1d900322203685d84be22fe2baf687ced526955a
faridsc <farid92.scorpio@gmail.com>
2023-04-04 02:47:41 +0200

    task 6

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "defs.h"
+
+/**
+ * free_listint2 - frees the list memory
+ * @head: the list head ponter
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (*head == NULL)
+	{
+		next = NULL;
+		return;
+	}
+	else
+	{
+		next = (*head)->next;
+		free(*head);
+		free_listint2(&next);
+	}
+}
