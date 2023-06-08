5e584b65bab2a2bae7a07cd5fe3e7b84b22c1f1f
faridsc <farid92.scorpio@gmail.com>
2023-06-08 20:07:59 +0100

    5-sqrt_recursion.c added

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,23 @@
-#include <math.h>
+/**
+ * root - claculates the root square with a recursion and a base num
+ * a healp function for the _sqrt_recursion function
+ * @n: the root to test if @n * @n is @b
+ * @b: the base number
+ * Return: int
+ */
+int root(int n, int b)
+{
+	/*
+	 * when n bigger than the b number means that
+	 * there is no natural square root
+	 */
+	if (n * n > b)
+		return (-1);
+	if (n * n == b)
+		return (n);
+	return (root(n + 1, b));
+}
+
 /**
  * _sqrt_recursion - calculates the sqrt of @n
  * @n: int to calculates its sqrt
@@ -6,13 +25,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	for (i = 0; i <= (n / 2 + (n % 2)); i++)
-	{
-		if (n == i * i)
-			return (i);
-	}
-	return (-1);
+	/*
+	 * return the root helping function results
+	 * the @n argument of the root function starts with 1 value
+	*/
+	return (root(1, n));
 }
-
